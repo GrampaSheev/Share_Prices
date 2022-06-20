715c78de77d0d0a9fd268cfeb293711967c7f9dd
GrampaSheev <94834543+GrampaSheev@users.noreply.github.com>
2022-06-20 09:16:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,44 @@ import yfinance as yf
 Y = "Y"
 N = "N"
 
+#What share and has shares
 share = yf.Ticker(input("What share would you like?(Enter the Ticker/Stock Symbol ex: msft or appl)"))
 HaveShares = input("Do you have any shares? Y/N")
 
-amount = share.info["currentPrice"]
-url = f"https://api.apilayer.com/exchangerates_data/convert?to=GBP&from=USD&amount={amount}"
+#Currencies options
+to = input("Choose currency to convert too. Choises are USD/GBP/EUR")
+from = input("Choose currency to convert from. Choises are USD/GBP/EUR")
 
+#Ticker and Exchange rates
+amount = share.info["currentPrice"]
+url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}"
+
+#Currencies
+USD = "$"
+GBP = "£"
+EUR = "€"
+
+#Auth
 payload = {}
 headers= {
   "apikey": "SylZpkQ5hnB3Dovy5adcj5OukR9hOEFV"
 }
 
+#API request
 response = requests.request("GET", url, headers=headers, data = payload)
 
 status_code = response.status_code
 result = response.text
 data = response.json()
 
+
+#Owned
 if HaveShares.lower() == Y.lower():
   SharesNum = float(input("Enter numner of shares"))
   AssetsOwned = SharesNum * data["result"] 
-  print(AssetsOwned) 
+  print(to, AssetsOwned) 
   
 
-
+#Unowned
 if HaveShares.lower() == N.lower():
-  print(data["result"])
+  print(to, data["result"])
